1b0286f391cc625f62b5e3b90076ebffb23f897a
Leonardo Candusso <leonardocandusso@gmail.com>
2026-03-17 21:48:03 -0300

    Adicionei API: spotify-web-api-node

--- src/index.js
+++ src/index.js
@@ -1,21 +1,21 @@
 require("dotenv").config();
-
-// Importando a biblioteca do servidor
 const express = require("express");
 
-// 3. Inicializando o servidor
-const app = express();
+const SpotifyAPI = require("spotify-web-api-node");
 
-// 4. Criando uma "rota" (Uma porta de entrada)
-// Quando alguém acessar http://127.0.0.1:3000/callback, ela responde isso:
-app.get("/callback", (requisicao, resposta) => {
-  resposta.send("Olá! Aqui está é spotify.");
+const spotifyApi = new SpotifyAPI({
+  clientId: process.env.SPOTIFY_CLIENT_ID,
+  clientSecret: process.env.SPOTIFY_CLIENT_SECRET,
+  redirectUri: "http://127.0.0.1:3000/callback",
 });
 
-// 5. Ligando o servidor e dizendo em qual porta ele deve escutar
+const app = express();
+
+app.get("/callback", (requisicao, resposta) => {
+  resposta.send("Olá! Aqui está é Spotify");
+});
+
 const PORTA = 3000;
 app.listen(PORTA, () => {
   console.log(`teste spotify ${PORTA}...`);
-  // Só pra provar que o cofre abriu, vou imprimir uma senha fake aqui:
-  // console.log("Minha senha secreta é:", process.env.SENHA_DA_PIZZARIA);
 });
