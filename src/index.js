8dc9cb0dd654fa412c0683448c6aa8429c6d5878
Leonardo Candusso <leonardocandusso@gmail.com>
2026-03-18 12:48:21 -0300

    Reorganiza rotas e adiciona try/catch no callback

--- src/index.js
+++ src/index.js
@@ -1,7 +1,15 @@
+// ==========================================
+// BLOCO 1: IMPORTAÇÕES
+// ==========================================
+
 require("dotenv").config();
 const express = require("express");
-
 const SpotifyAPI = require("spotify-web-api-node");
+
+// ==========================================
+// BLOCO 2: CONFIGURAÇÕES
+// Inicialização de variáveis globais e conexões.
+// ==========================================
 
 const spotifyApi = new SpotifyAPI({
   clientId: process.env.SPOTIFY_CLIENT_ID,
@@ -11,11 +19,9 @@ const spotifyApi = new SpotifyAPI({
 
 const app = express();
 
-app.get("/callback", (requisicao, resposta) => {
-  resposta.send("Olá! Aqui está é Spotify");
-});
-
-// --- LOGIN ---
+// ==========================================
+// BLOCO 3: ROTAS
+// ==========================================
 
 app.get("/login", (requisicao, resposta) => {
   const permissoesDoSpotify = [
@@ -30,7 +36,27 @@ app.get("/login", (requisicao, resposta) => {
   resposta.redirect(linkDeLogin);
 });
 
+app.get("/callback", async (requisicao, resposta) => {
+  try {
+    const codigoSpotify = requisicao.query.code;
+    const dadosDoSpotify =
+      await spotifyApi.authorizationCodeGrant(codigoSpotify);
+    const meuAccessToken = dadosDoSpotify.body.access_token;
+    console.log("MEU ACCESS TOKEN É:", meuAccessToken);
+
+    resposta.send("Troca concluída! Olhe o seu terminal.");
+  } catch (erro) {
+    console.log("Ops, deu um problema:", erro);
+    resposta.send("Houve um erro na comunicação.");
+  }
+});
+
+// ==========================================
+// BLOCO 4: INICIALIZAÇÃO
+// O comando de escutar a porta fica SEMPRE no final do arquivo.
+// ==========================================
+
 const PORTA = 3000;
 app.listen(PORTA, () => {
-  console.log(`teste spotify ${PORTA}...`);
+  console.log(`teste porta: ${PORTA}...`);
 });
